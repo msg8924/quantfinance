d03c8775cd92d080dda7f2cd6911e3e3b0064718
msg8924 <45294471+msg8924@users.noreply.github.com>
2023-09-18 16:52:56 -0400

    Removed the GradientDescent implementation. Will need to fix it later

--- DataScience/UnivariateLinearRegression.py
+++ DataScience/UnivariateLinearRegression.py
@@ -1,11 +1,6 @@
 import numpy as np
 
 class UnivariateLinearRegression:
-    def __init__(self,iterations, learning_rate):
-        self.iterations = iterations
-        self.learning_rate = learning_rate
-
-
     def estimate_coefficients(self,x, y):
         c = x.shape[0]
         m_x = np.mean(x)
@@ -17,32 +12,6 @@ class UnivariateLinearRegression:
         b_0 = m_y - (b_1 * m_x)
         return b_0, b_1
 
-
-    def fit(self, X, Y):
-        self.r = X.shape[0]
-        self.W = np.zeros(self.r)
-        self.b = 0
-        self.X = X
-        self.Y = Y
-
-        for i in range(self.iterations):
-            self.update_weights()
-
-        return self
-
-    def update_weights(self):
-        Y_pred = self.predict(self.X)
-        dW = - (2 * (self.X.T).dot(self.Y - Y_pred)) / self.r
-        db = - 2 * np.sum(self.Y - Y_pred) / self.r
-        #W is the bet and b is the intercept
-        self.W = self.W - (self.learning_rate * dW)
-        self.b = self.b - (self.learning_rate * db)
-        return self
-
-    def predict(self, X):
-        return X.dot(self.W) + self.b
-
-
     def test(self):
         # observations / data
         x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -52,8 +21,6 @@ class UnivariateLinearRegression:
         b = self.estimate_coefficients(x, y)
         print("Estimated coefficients: b_0 = {} & b_1 = {}".format(b[0], b[1]))
 
-
-
 if __name__ == "__main__":
-    reg = UnivariateLinearRegression(iterations = 10, learning_rate = 0.01)
+    reg = UnivariateLinearRegression()
     reg.test()
