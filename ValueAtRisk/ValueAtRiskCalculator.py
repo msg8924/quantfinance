1e8c6e3368081f92644ba9f2017ee78ca0049253
msg8924 <45294471+msg8924@users.noreply.github.com>
2023-10-16 06:47:49 -0400

    Fixed Stock Price Simulation Calculation

--- ValueAtRisk/ValueAtRiskCalculator.py
+++ ValueAtRisk/ValueAtRiskCalculator.py
@@ -19,14 +19,14 @@ class ValueAtRiskCalculator:
 
     def simulate(self, iterations, initial_stock_price):
         W = random.normal(0,1,[1,iterations])
-        stock_prices = initial_stock_price * exp(((self.mu - self.sigma**2/2)*self.n) - (self.sigma * sqrt(self.n) *W))
+        stock_prices = initial_stock_price * exp(((self.mu - self.sigma**2/2)*self.n) + (self.sigma * sqrt(self.n) *W))
         stock_prices = sort(stock_prices)
         stock_price = percentile(stock_prices, (1-self.c) * 100)
         qty = self.position / initial_stock_price
         return abs(qty * (initial_stock_price - stock_price))
 
 if __name__ == '__main__':
-    calculator = ValueAtRiskCalculator(1e6, 0.01, 0.0005,0.99, 10)
+    calculator = ValueAtRiskCalculator(1e6, 0.05, 0.005,0.99, 10)
     print("With {:.2f} confidence, the portfolio will not lose more than ${:.2f} over the"
           " next {} day(s)".format(calculator.c, calculator.compute_value_at_risk(), calculator.n))
     print("With {:.2f} confidence, the portfolio will not lose more than ${:.2f} over the"
